6c0962b0e54b913350f20dce36f9c15541de3fd8
eunuj <eunuj0856@gmail.com>
2023-08-07 17:45:49 +0900

    chore (all) : ui design

--- app/src/main/java/com/example/firststep/calendar.kt
+++ app/src/main/java/com/example/firststep/calendar.kt
@@ -2,24 +2,10 @@ package com.example.firststep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-
-
 
 class calendar : AppCompatActivity() {
-
-    private lateinit var binding : activityCalendar
-
-    private lateinit var db : AppDatabaase
-
-//calendarBinding 선언 안됨 오류남, 그냥 calendar 하면 inflate 안됨
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = activityCalendar.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
+        setContentView(R.layout.activity_calendar)
     }
 }
