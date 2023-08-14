1cfd04d12a51ed3214670c90c990d2d111b9ca21
mmmamama <akpepewo@gmail.com>
2023-08-14 18:05:17 +0900

    feat(login): id,password

--- app/src/main/java/com/example/firststep/login.kt
+++ app/src/main/java/com/example/firststep/login.kt
@@ -1,11 +1,14 @@
 package com.example.firststep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class login : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
