2e3572065bdacaa5e2d1df2648576777e81f65f0
joyw03 <tomybaby1@naver.com>
2023-11-14 15:04:11 +0900

    feat(usergoalinfo) : create layout

--- app/src/main/java/com/example/firststep/account.kt
+++ app/src/main/java/com/example/firststep/account.kt
@@ -1,16 +1,27 @@
 package com.example.firststep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class account : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+        accountToUserGoalInfoButton()
     }
 
     override fun onPause() {
         super.onPause()
         overridePendingTransition(0, 0) // 애니메이션 비활성화
+    }
+
+    private fun accountToUserGoalInfoButton() {
+        val button = findViewById<Button>(R.id.btn_goto_UserGoalInfo)
+        button.setOnClickListener {
+            val intent = Intent(this,UserGoalInfo::class.java)
+            startActivity(intent)
+        }
     }
 }
